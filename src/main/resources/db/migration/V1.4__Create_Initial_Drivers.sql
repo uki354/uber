da87afe986546968fd8d477a5e5f22a1f97c3410
uki <uros.3.stevanovic@gmail.com>
2022-03-22 16:10:46 +0100

    Add flyway migration to initilaize drivers
    Inserts initial drivers info into db

---
+++ src/main/resources/db/migration/V1.4__Create_Initial_Drivers.sql
@@ -0,0 +1,27 @@
+INSERT INTO `uber`.`driver_model`
+    (`driver_id`,`is_deleted`, `created_at`, `first_name`, `last_name`, `birthdate`, `image_path`, `mobile_phone`, `email`, `address`, `jmbg`, `gender`)
+    VALUES ('1','0', '2020-02-02 10:00:00', 'Milan', 'Peric', '1999-02-02', '', '123231232', 'milan@gmail.com', 'adress', '123243', '0');
+
+INSERT INTO `uber`.`driver_model`
+(`driver_id`,`is_deleted`, `created_at`, `first_name`, `last_name`, `birthdate`, `image_path`, `mobile_phone`, `email`, `address`, `jmbg`, `gender`)
+VALUES ('2','0', '2020-02-02 10:00:00', 'Goran', 'Bregovic', '1999-02-02', '', '4534534', 'goran@gmail.com', 'adress', '32312312', '0');
+
+INSERT INTO `uber`.`driver_model`
+(`driver_id`,`is_deleted`, `created_at`, `first_name`, `last_name`, `birthdate`, `image_path`, `mobile_phone`, `email`, `address`, `jmbg`, `gender`)
+VALUES ('3','0', '2020-02-02 10:00:00', 'Milos', 'Stojanovic', '1999-02-02', '', '54343433', 'milos@gmail.com', 'adress', '3454654', '0');
+
+INSERT INTO `uber`.`driver_model`
+(`driver_id`,`is_deleted`, `created_at`, `first_name`, `last_name`, `birthdate`, `image_path`, `mobile_phone`, `email`, `address`, `jmbg`, `gender`)
+VALUES ('4','0', '2020-02-02 10:00:00', 'Jovana', 'Simic', '1999-02-02', '', '435345432', 'jovana@gmail.com', 'adress', '6575675', '1');
+
+INSERT INTO `uber`.`driver_model`
+(`driver_id`,`is_deleted`, `created_at`, `first_name`, `last_name`, `birthdate`, `image_path`, `mobile_phone`, `email`, `address`, `jmbg`, `gender`)
+VALUES ('5','0', '2020-02-02 10:00:00', 'Dusan', 'Peric', '1999-02-02', '', '434323222', 'dusan@gmail.com', 'adress', '6546456', '0');
+
+INSERT INTO `uber`.`driver_model`
+(`driver_id`,`is_deleted`, `created_at`, `first_name`, `last_name`, `birthdate`, `image_path`, `mobile_phone`, `email`, `address`, `jmbg`, `gender`)
+VALUES ('6','0', '2020-02-02 10:00:00', 'Milena', 'Lazic', '1999-02-02', '', '434321876', 'milena@gmail.com', 'adress', '534534', '1');
+
+INSERT INTO `uber`.`driver_model`
+(`driver_id`,`is_deleted`, `created_at`, `first_name`, `last_name`, `birthdate`, `image_path`, `mobile_phone`, `email`, `address`, `jmbg`, `gender`)
+VALUES ('7','0', '2020-02-02 10:00:00', 'Marko', 'Stojic', '1999-02-02', '', '76875733', 'marko@gmail.com', 'adress', '435345', '0');
